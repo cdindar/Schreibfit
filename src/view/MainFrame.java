3aec678f2d9ab86a7282374019de5dc00482017d
cdindar <cdindar@student.tgm.ac.at>
2026-02-15 16:14:10 +0100

    Klasse MainFrame etwas angepasst

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -15,7 +15,7 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        JPanel root = new JPanel(new BorderLayout());
+        JPanel root = new JPanel(new GridLayout(2, 1));
         root.setBackground(BEIGE);
 
         JPanel header = new JPanel(new BorderLayout());

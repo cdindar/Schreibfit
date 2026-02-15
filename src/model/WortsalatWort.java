25147cf8e8a7709320d5f917dcbb1cf89d58d886
BajsangurOzdiev <bisaewa@student.tgm.ac.at>
2026-02-15 21:04:27 +0100

    Klasse WortsalatWort mit einem konstruktor und keinen methoden

---
+++ src/model/WortsalatWort.java
@@ -0,0 +1,11 @@
+package model;
+
+public class WortsalatWort {
+    public String wort;
+    public String hinweis;
+
+    public WortsalatWort(String wort, String hinweis) {
+        this.wort = wort;
+        this.hinweis = hinweis;
+    }
+}
